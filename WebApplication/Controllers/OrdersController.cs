d7f97c63ed81b4311ddbad76af13d57ff2e80b72
mccrackenn <mccrackenn@gmail.com>
2021-05-05 16:54:40 -0700

    Add project files.

---
+++ WebApplication/Controllers/OrdersController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity.Validation;
+using System.Net.Http;
+using System.Web.Http;
+using HttpGetAttribute = System.Web.Http.HttpGetAttribute;
+using RoutePrefixAttribute = System.Web.Http.RoutePrefixAttribute;
+
+namespace WebApplication.Controllers
+{
+    [RoutePrefix("api/orders")]
+    public class OrdersController : ApiController
+    {
+        // GET: Orders
+        NodeOrders500Entities OrdersEntity = new NodeOrders500Entities();
+
+        public IEnumerable<object> GetSalesPeople()
+        {
+            var salesPeople = from person in OrdersEntity.SalesPersonTables
+                              orderby person.LastName
+                              select new { person.FirstName, person.LastName };
+
+            return salesPeople.ToList();
+        }
+
+        [System.Web.Http.Route("getcities")]
+        [HttpGet]
+        public IEnumerable<object> GetCities()
+        {
+            var cities = from city in OrdersEntity.StoreTables
+                         orderby city.City
+                         select new { city.City };
+
+            return cities.ToList();
+        }
+
+        [System.Web.Http.Route("getmarkupresults")]
+        [HttpGet]
+        public IEnumerable<object> GetMarkupResults()
+        {
+            var results = from result in OrdersEntity.Orders
+                          where result.pricePaid > 13
+                          select new { result.StoreTable.City };
+
+            var grouped = from a in results
+                          group a by a.City;
+
+            return grouped.ToList();
+        }
+    }
+}
---
+++ WebApplication/Scripts/main.js
@@ -0,0 +1,66 @@
+﻿var uri = 'api/Orders';
+myCityArray = [];
+mySalesArray = [];
+myMarkupArray = [];
+tempMarkupArray = [];
+
+/*https://nodogmablog.bryanhogan.net/2016/10/web-api-2-controller-with-multiple-get-methods/*/
+
+$(document).ready(function () {
+    fetch(uri).then(function (response) {
+        if (response.ok) {
+            return response.json();
+        } else {
+            return Promise.reject(response);
+        }
+
+    }).then(function (salesData) {
+        console.log(salesData);
+        mySalesArray = salesData;
+        $.each(salesData, function (key, item) {
+            $('#select-people').append($('<option></option').val(item.$id).html(item.FirstName +" "+item.LastName));
+        });
+        return fetch(uri + '/getcities');
+    }).then(function (response) {
+        if (response.ok) {
+            return response.json();
+        } else {
+            return Proomise.reject(response);
+        }
+    }).then((cityData) => {
+        myCityArray = cityData;
+        console.log(cityData);
+        $.each(cityData, function (key, item) {
+            $('#select-cities').append($('<option></option').val(item.$id).html(item.City));
+        });
+    })
+});
+
+GetMarkups = () => {
+    fetch(uri + '/getmarkupresults').then(function (response) {
+        if (response.ok) {
+            return response.json();
+        } else {
+            return Promise.reject(response);
+        }
+    }).then((markupData) => {
+        console.log(markupData);
+        tempMarkupArray = markupData;
+        myMarkupArray = mySort(tempMarkupArray);
+        console.log(myMarkupArray);
+        $.each(myMarkupArray, function (key, item) {
+
+            var $myVar = $("<li>City:"+ item[0].City +", Count:" +item.length +"</li>");
+            $('#markup-list').append($myVar);
+
+        });
+    })
+}
+mySort = (anArray) => {
+    anArray.sort(function (a, b) {
+        if (a.length < b.length) return 1;
+        if (a.length > b.length) return -1;
+        return 0;
+    });
+    return anArray;
+}
